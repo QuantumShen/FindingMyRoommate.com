b747ec9e863caf36a4ca4ed3377889b6fb936425
quantumshen <shenguangtong@yeah.net>
2016-08-14 02:24:17 -0400

    Redirect to login form when not logged in; Use bootstrap account template

---
+++ client/partials/ServiceEntrance.js
@@ -0,0 +1,34 @@
+Template.ServiceEntrance.helpers({
+    services:[
+        {
+            path:"/rides",
+            imgPath:"/images/share-a-ride.png",
+            title: "Share A Ride",
+            chineseTitle: "拼车",
+            description:"Start or join a ride share",
+            chineseDescription: "发布或寻找拼车信息",
+            activeName: "Active Rides",
+            activeNumber: 10, //todo
+        },
+        {
+            path:"/roommates",
+            imgPath:"/images/share-a-ride.png",
+            title: "Find Roommates",
+            chineseTitle: "找室友",
+            description:"Start or join a roommates group",
+            chineseDescription: "找室友一起租房子",
+            activeName: "Active Groups",
+            activeNumber: 10, //todo
+        },{
+            path:"/subleases",
+            imgPath:"/images/share-a-ride.png",
+            title: "Sublease Information",
+            chineseTitle: "转租",
+            description:"Provide or ask for sublease",
+            chineseDescription: "发布或寻找拼车信息",
+            activeName: "Active Subleases",
+            activeNumber: 10, //todo
+        },
+
+    ]
+});
